326023c7817449164b0dd03179a2802dc17f9edd
Attula15 <100715516+Attula15@users.noreply.github.com>
2023-10-10 20:51:34 +0200

    first commit

---
+++ src/test/java/com/bence/fileshare/FileshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bence.fileshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
